4f7c2f9bf35227dc80dce56113ba081e81abb026
yitian9898 <yitian9898@gmail.com>
2018-10-31 14:03:21 -0400

    fixed-unnecessary-macro-definition

--- src/main/java/edu/cmu/cs/cs214/rec10/plugin/SortWithSwapsPlugin.java
+++ src/main/java/edu/cmu/cs/cs214/rec10/plugin/SortWithSwapsPlugin.java
@@ -6,8 +6,8 @@ import edu.cmu.cs.cs214.rec10.framework.core.GamePlugin;
 import java.util.Random;
 
 public class SortWithSwapsPlugin implements GamePlugin {
-    private int ONE = 1;
-    private int TEN = 10;
+    private int one = 1;
+    private int ten = 10;
 
     // Describes whether the current move is over
     public Boolean move = false;
